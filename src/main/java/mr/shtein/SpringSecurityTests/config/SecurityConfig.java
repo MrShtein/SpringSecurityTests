6d68e14ca39a91a5450d76771dcfc0831bd67384
Mr.Shtein <mr.shtein61@yandex.ru>
2021-07-21 15:07:41 +0300

    Configured http security obj

--- src/main/java/mr/shtein/SpringSecurityTests/config/SecurityConfig.java
+++ src/main/java/mr/shtein/SpringSecurityTests/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package mr.shtein.SpringSecurityTests.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -13,12 +14,24 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import mr.shtein.SpringSecurityTests.model.ROLE;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/**").hasAnyRole(ROLE.ADMIN.name(), ROLE.USER.name())
+                .antMatchers(HttpMethod.POST, "/api/**").hasRole(ROLE.ADMIN.name())
+                .antMatchers(HttpMethod.DELETE, "/api/**").hasRole(ROLE.ADMIN.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 
     @Bean

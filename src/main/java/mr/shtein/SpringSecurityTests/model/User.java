f2a7d70dbbe06e3c45aa4dd8e6b6b3ea07990ec3
Mr.Shtein <mr.shtein61@yandex.ru>
2021-07-21 17:09:31 +0300

    Added userModel

---
+++ src/main/java/mr/shtein/SpringSecurityTests/model/User.java
@@ -0,0 +1,42 @@
+package mr.shtein.SpringSecurityTests.model;
+
+import javax.management.relation.Role;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.Generated;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+
+    @Column(name = "status")
+    private Status status;
+}
